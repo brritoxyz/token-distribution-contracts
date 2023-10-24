17b7ad4541dc2cf587586a75a549a60a19d26034
kphed <kpcrypto69@gmail.com>
2023-10-24 11:29:35 -0400

    Update deployment script

--- script/TokenClaims.s.sol
+++ script/TokenClaims.s.sol
@@ -2,11 +2,20 @@
 pragma solidity 0.8.19;
 
 import "forge-std/Script.sol";
+import {BetaTesters} from "src/BetaTesters.sol";
+import {RumasOwners} from "src/RumasOwners.sol";
+import {RumasSetOwners} from "src/RumasSetOwners.sol";
+import {RumasUniqueOwners} from "src/RumasUniqueOwners.sol";
 
 contract TokenClaimsScript is Script {
-    function setUp() public {}
-
     function run() public {
-        vm.broadcast();
+        vm.startBroadcast(vm.envUint("PRIVATE_KEY"));
+
+        new BetaTesters();
+        new RumasOwners();
+        new RumasSetOwners();
+        new RumasUniqueOwners();
+
+        vm.stopBroadcast();
     }
 }
